0b333a095c81dc0df1d675b98e9f2c46e5e0252c
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2017-08-07 20:35:19 +0200

    norm x

--- srcs/handle_x.c
+++ srcs/handle_x.c
@@ -6,11 +6,53 @@
 /*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 17:26:47 by msakwins          #+#    #+#             */
-/*   Updated: 2017/06/22 14:35:49 by msakwins         ###   ########.fr       */
+/*   Updated: 2017/08/07 20:34:28 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+static int		handle_no_minus(t_modif *modi, char *base, uintmax_t nb)
+{
+	int ret;
+
+	ret = 0;
+	if (SHARP && nb > 0 && !PRECI && ZERO)
+		ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
+	if (DIGIT && !PRECI)
+		ret += apply_digits(modi);
+	if (SHARP && nb > 0 && !PRECI && !ZERO)
+		ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
+	ft_putnbr_base(nb, base);
+	return (ret);
+}
+
+static int		handle_flags(t_modif *modi, int nblen, char *base, uintmax_t nb)
+{
+	int ret;
+
+	ret = 0;
+	if (DIGIT || PRECI)
+		width_errors(modi, nblen);
+	if (MINUS)
+	{
+		if (SHARP && nb > 0)
+			ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
+		ft_putnbr_base(nb, base);
+		ret += apply_digits(modi);
+	}
+	if (PRECI > 0)
+	{
+		ret += apply_digits(modi);
+		if (SHARP && nb > 0)
+			ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
+		ret += apply_preci(modi);
+	}
+	if (!MINUS)
+		ret += handle_no_minus(modi, base, nb);
+	ret += nblen;
+	return (ret);
+}
 
 int				handle_x(va_list argl, t_modif *modi)
 {
@@ -37,36 +79,7 @@ int				handle_x(va_list argl, t_modif *modi)
 		ret += apply_digits(modi);
 		return (ret);
 	}
-	if (DIGIT || PRECI)
-	{
-		width_errors(modi, nblen);
-	}
-	if (MINUS)
-	{
-		if (SHARP && nb > 0)
-			ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
-		ft_putnbr_base(nb, base);
-		ret += apply_digits(modi);
-	}
-	if (PRECI > 0)
-	{
-		ret += apply_digits(modi);
-		if (SHARP && nb > 0)
-			ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
-		ret += apply_preci(modi);
-	}
-	if (!MINUS)
-	{
-		if (SHARP && nb > 0 && !PRECI && ZERO)
-			ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
-		if (DIGIT && !PRECI)
-			ret += apply_digits(modi);
-		if (SHARP && nb > 0 && !PRECI && !ZERO)
-			ret += (modi->cap) ? get_strlen("0X") : get_strlen("0x");
-		ft_putnbr_base(nb, base);
-	}
-	ret += nblen;
-	return (ret);
+	return (handle_flags(modi, nblen, base, nb));
 }
 
 uintmax_t		to_unsigned_char_modulo(uintmax_t nbr)
