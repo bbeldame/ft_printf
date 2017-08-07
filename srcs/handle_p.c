4e8de6a1a1b0c31768734bb67cfb73e96302febf
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2017-08-07 20:48:11 +0200

    norm p

--- srcs/handle_p.c
+++ srcs/handle_p.c
@@ -6,27 +6,17 @@
 /*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/11 17:41:39 by msakwins          #+#    #+#             */
-/*   Updated: 2017/06/22 14:26:32 by msakwins         ###   ########.fr       */
+/*   Updated: 2017/08/07 20:46:10 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int			handle_p(va_list argl, t_modif *modi)
+static int	handle_flags(t_modif *modi, uintmax_t nb, int nblen)
 {
-	int				ret;
-	uintmax_t		nb;
-	int				nblen;
+	int ret;
 
 	ret = 0;
-	modi->hexa = 1;
-	nb = (uintmax_t)va_arg(argl, void*);
-	nblen = get_uintlen(nb, HEXA_MIN);
-	DIGIT -= 2;
-	PLUS = 0;
-	SPACE = 0;
-	if (period_zero(nb, modi))
-		return (ret + get_strlen("0x"));
 	if (PRECI || DIGIT)
 		width_errors(modi, nblen);
 	if (SHARP == 1 && nb > 0)
@@ -47,5 +37,24 @@ int			handle_p(va_list argl, t_modif *modi)
 	if (!MINUS && !ZERO)
 		ft_putnbr_base(nb, HEXA_MIN);
 	ret += get_uintlen(nb, HEXA_MIN);
+	return (ret);
+}
+
+int			handle_p(va_list argl, t_modif *modi)
+{
+	int				ret;
+	uintmax_t		nb;
+	int				nblen;
+
+	ret = 0;
+	modi->hexa = 1;
+	nb = (uintmax_t)va_arg(argl, void*);
+	nblen = get_uintlen(nb, HEXA_MIN);
+	DIGIT -= 2;
+	PLUS = 0;
+	SPACE = 0;
+	if (period_zero(nb, modi))
+		return (ret + get_strlen("0x"));
+	ret += handle_flags(modi, nb, nblen);
 	return (ret);
 }
