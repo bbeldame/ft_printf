f8d94cafc705f1ab6a7b74acaeafd1e9099f0309
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2017-08-07 20:43:51 +0200

    norm s

--- srcs/handle_s.c
+++ srcs/handle_s.c
@@ -6,48 +6,51 @@
 /*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/27 19:40:54 by msakwins          #+#    #+#             */
-/*   Updated: 2017/06/22 14:26:55 by msakwins         ###   ########.fr       */
+/*   Updated: 2017/08/07 20:40:44 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-size_t		handle_s(va_list argl, t_modif *modi)
+static int	handle_period_or_digit(t_modif *modi, char *str)
 {
-	int			ret;
-	char		*str;
-	int			slen;
-	int			nblen;
+	int		ret;
+	int		slen;
+	int		nblen;
 
 	ret = 0;
 	slen = 0;
 	nblen = 0;
+	slen = ft_strlen(str);
+	nblen = PERIOD && PRECI < slen ? PRECI : slen;
+	width_errs(modi, nblen);
+	if (DIGIT && !MINUS)
+		ret += apply_digits(modi);
+	if (PERIOD == 1 && PRECI >= 0 && PRECI < slen)
+	{
+		ret += PRECI;
+		ft_putnstr(str, PRECI);
+	}
+	else
+	{
+		ft_putstr(str);
+		ret += slen;
+	}
+	if (DIGIT && MINUS)
+		ret += apply_digits(modi);
+	return (ret);
+}
+
+size_t		handle_s(va_list argl, t_modif *modi)
+{
+	int			ret;
+	char		*str;
+
+	ret = 0;
 	str = va_arg(argl, char *);
 	str = str == NULL ? "(null)" : str;
 	if (PERIOD || DIGIT)
-	{
-		slen = ft_strlen(str);
-		nblen = PERIOD && PRECI < slen ? PRECI : slen;
-		width_errs(modi, nblen);
-		if (DIGIT && !MINUS)
-		{
-			ret += apply_digits(modi);
-		}
-		if (PERIOD == 1 && PRECI >= 0 && PRECI < slen)
-		{
-			ret += PRECI;
-			ft_putnstr(str, PRECI);
-		}
-		else
-		{
-			ft_putstr(str);
-			ret += slen;
-		}
-		if (DIGIT && MINUS)
-		{
-			ret += apply_digits(modi);
-		}
-	}
+		ret += handle_period_or_digit(modi, str);
 	else
 		ret += get_strlen(str);
 	return (ret);
