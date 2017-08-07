f58c5dcff6a36988b4e4d04b6255debda8273639
Basile BELDAME <basile.beldame@supdeweb-paris.com>
2017-08-07 20:56:58 +0200

    norm w

--- srcs/handle_w.c
+++ srcs/handle_w.c
@@ -6,11 +6,24 @@
 /*   By: bbeldame <bbeldame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/15 19:39:24 by msakwins          #+#    #+#             */
-/*   Updated: 2017/06/22 14:30:02 by msakwins         ###   ########.fr       */
+/*   Updated: 2017/08/07 20:56:16 by bbeldame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+static int	handle_w_minus(t_modif *modi, wchar_t value, int size)
+{
+	int ret;
+
+	ret = 0;
+	ret += apply_preci(modi);
+	if (!modi->cap && !modi->mod)
+		ret += get_charlen(value);
+	else
+		ret += ft_putwchar(value, size);
+	return (ret);
+}
 
 int			handle_w(va_list argl, t_modif *modi)
 {
@@ -27,13 +40,7 @@ int			handle_w(va_list argl, t_modif *modi)
 	nblen = size <= 7 ? 1 : size / 8;
 	width_errors(modi, nblen);
 	if (MINUS)
-	{
-		ret += apply_preci(modi);
-		if (!modi->cap && !modi->mod)
-			ret += get_charlen(value);
-		else
-			ret += ft_putwchar(value, size);
-	}
+		ret += handle_w_minus(modi, value, size);
 	if (DIGIT > 0)
 		ret += apply_digits(modi);
 	if (!MINUS)
@@ -46,16 +53,43 @@ int			handle_w(va_list argl, t_modif *modi)
 	return (ret);
 }
 
+static int	ws_flags(t_modif *modi, wchar_t *wstr, int slen)
+{
+	int ret;
+	int wlen;
+
+	ret = 0;
+	wlen = 0;
+	if (PERIOD == 1 && PRECI < slen && PRECI > 0)
+		wlen = get_preciw(wstr, PRECI);
+	else
+		wlen = slen;
+	if (PERIOD == 1 && PRECI == 0)
+		wlen = 0;
+	if (PERIOD == 0 && MINUS)
+		ret += ft_putwstr(wstr);
+	if (DIGIT > wlen)
+	{
+		DIGIT = DIGIT - wlen;
+		ret += apply_digits(modi);
+		if (PERIOD == 0 && !MINUS)
+			ret += ft_putwstr(wstr);
+	}
+	if (PERIOD == 1 && PRECI > 0 && PRECI <= slen)
+	{
+		ret += ft_putwnstr(wstr, PRECI);
+	}
+	return (ret);
+}
+
 int			handle_ws(va_list argl, t_modif *modi)
 {
 	int				ret;
 	wchar_t			*wstr;
 	int				slen;
-	int				wlen;
 
 	ret = 0;
 	slen = 0;
-	wlen = 0;
 	wstr = va_arg(argl, wchar_t*);
 	if (wstr == NULL)
 	{
@@ -64,24 +98,7 @@ int			handle_ws(va_list argl, t_modif *modi)
 	}
 	slen = get_wstrlen(wstr);
 	if (PERIOD == 1 || DIGIT > 0)
-	{
-		wlen = PERIOD == 1 && PRECI < slen && PRECI > 0 ? get_preciw(wstr, PRECI) : slen;
-		if (PERIOD == 1 && PRECI == 0)
-			wlen = 0;
-		if (PERIOD == 0 && MINUS)
-			ret += ft_putwstr(wstr);
-		if (DIGIT > wlen)
-		{
-			DIGIT = DIGIT - wlen;
-			ret += apply_digits(modi);
-			if (PERIOD == 0 && !MINUS)
-				ret += ft_putwstr(wstr);
-		}
-		if (PERIOD == 1 && PRECI > 0 && PRECI <= slen)
-		{
-			ret += ft_putwnstr(wstr, PRECI);
-		}
-	}
+		ret += ws_flags(modi, wstr, slen);
 	else
 		ret += ft_putwstr(wstr);
 	return (ret);
